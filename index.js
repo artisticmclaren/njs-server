e12906904422b7314de28cc35ca439a35bdb21e8
artisticmclaren <104241235+artisticmclaren@users.noreply.github.com>
2024-03-08 15:15:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require("node:http");
+const hostname = "0.0.0.0";
+const port = 3000;
+const fs = require("node:fs");
+
+const server = http.createServer((req, res) => {
+  let url = "";
+
+  if (req.url === "/") {
+    console.log("res/index.html");
+    url = "res/index.html";
+  } else {
+    console.log(`res${req.url}`);
+    url = `res${req.url}`;
+  }
+
+  if (req.url.endsWith("#download")) {
+    const file = fs.createWriteStream(req.url);
+    const request = http.get(req.url, function (response) {
+      response.pipe(file);
+      file.on("finish", () => {
+        file.close();
+        console.log(`Downloaded ${req.url}`);
+      });
+    });
+  }
+
+  res.setHeader("Content-Type", "text/plain");
+
+  fs.readFile(url, "utf-8", (err, data) => {
+    if (err) {
+      res.statusCode = 404;
+      res.end(`404\n\n${err}`);
+      return;
+    }
+
+    console.log(data);
+    res.statusCode = 200;
+    res.end(data);
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
